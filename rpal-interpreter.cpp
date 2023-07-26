ad66236669c6b7d850bacd72b4ca6d4d48aa73a0
tharoosha <tharooshavihidun@gmail.com>
2023-07-26 10:05:51 +0530

    printing tree

--- rpal-interpreter.cpp
+++ rpal-interpreter.cpp
@@ -21,42 +21,83 @@ using namespace std;
 
 int main(int argc, char* argv[]){
     
-    string filename = argv[1];
+    if (argc == 3){
+        string filename = argv[2];
 
-    ifstream file(filename);
+        ifstream file(filename);
 
-    if (!file) {
-        cout << "Failed to open the file." << endl;
-        return 1;
-    };
+        if (!file) {
+            cout << "Failed to open the file." << endl;
+            return 1;
+        };
+        
+        scan(file); //Prepare the first token by placing it within 'NT'
+        E(file);    //Call the first non-terminal procedure to start parsing
+
+        if (checkIfEOF(file)) {
+        // cout << "\n\nEOF successfully reached after complete parsing! Will exit now!!\n\n";
+        // exit(1);
+            // runAndShowOutput();
+            printTree();
+            convertASTToStandardizedTree();
+            flattenStandardizedTree();
+            runCSEMachine();
+            cout << "\n";
+        } else {
+            cout <<"\n\nERROR! EOF not reached but went through the complete grammar! Will exit now!!\n\n";
+            exit(0);
+        };
+        // string fileContent;
+        // string line;
+        // while (getline(file, line)) {
+        //     fileContent += line + '\n';
+        // }
+
+        // cout << "File contents:\n" << fileContent << endl;
+
+        
+        file.close();
+
+        return 0;
+    }else {
+        string filename = argv[1];
+
+        ifstream file(filename);
+
+        if (!file) {
+            cout << "Failed to open the file." << endl;
+            return 1;
+        };
+        
+        scan(file); //Prepare the first token by placing it within 'NT'
+        E(file);    //Call the first non-terminal procedure to start parsing
+
+        if (checkIfEOF(file)) {
+        // cout << "\n\nEOF successfully reached after complete parsing! Will exit now!!\n\n";
+        // exit(1);
+            // runAndShowOutput();
+            convertASTToStandardizedTree();
+            flattenStandardizedTree();
+            runCSEMachine();
+            cout << "\n";
+        } else {
+            cout <<"\n\nERROR! EOF not reached but went through the complete grammar! Will exit now!!\n\n";
+            exit(0);
+        };
+        // string fileContent;
+        // string line;
+        // while (getline(file, line)) {
+        //     fileContent += line + '\n';
+        // }
+
+        // cout << "File contents:\n" << fileContent << endl;
+
+        
+        file.close();
+
+        return 0;
+    }
     
-    scan(file); //Prepare the first token by placing it within 'NT'
-    E(file);    //Call the first non-terminal procedure to start parsing
-
-    if (checkIfEOF(file)) {
-//                    cout << "\n\nEOF successfully reached after complete parsing! Will exit now!!\n\n";
-//                    exit(1);
-        // runAndShowOutput();
-        convertASTToStandardizedTree();
-        flattenStandardizedTree();
-        runCSEMachine();
-        cout << "\n";
-    } else {
-        cout <<"\n\nERROR! EOF not reached but went through the complete grammar! Will exit now!!\n\n";
-        exit(0);
-    };
-    // string fileContent;
-    // string line;
-    // while (getline(file, line)) {
-    //     fileContent += line + '\n';
-    // }
-
-    // cout << "File contents:\n" << fileContent << endl;
-
-    
-    file.close();
-
-    return 0;
 };
 
 
